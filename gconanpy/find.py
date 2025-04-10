30db4a52579d41f1c16030274c1eb0121c776829
Greg Conan <gregmconan@gmail.com>
2025-04-09 19:46:40 -0700

    Added comments; renamed WhittleUntil->ReadyChecker

--- gconanpy/find.py
+++ gconanpy/find.py
@@ -4,7 +4,7 @@
 Functions that iterate and break once they find what they're looking for.
 Greg Conan: gregmconan@gmail.com
 Created: 2025-04-02
-Updated: 2025-04-08
+Updated: 2025-04-09
 """
 # Import standard libraries
 import pdb
@@ -20,12 +20,11 @@ except ModuleNotFoundError:
 
 
 class BasicRange(Iterable):
-    """ Iterator like range() """
+    """ Iterator like range(); base class for custom iterators to add to. """
 
     def __init__(self, iter_over: Sequence[Typ.I], start_at: int = 0,
                  end_at: int | None = None, step: int = 1) -> None:
-        """ Iterator like range()
-
+        """ 
         :param iter_over: Sequence[Any] to iterate over
         :param start_at: int, iter_over index to begin at; defaults to 0
         :param end_at: int, iter_over index to stop at; defaults to len(iter_over)
@@ -43,14 +42,25 @@ class BasicRange(Iterable):
         return self.to_iter[ix]
 
     def __iter__(self):
+        """ Reset some variables to start iterating.
+
+        :return: BasicRange, self
+        """
         self.is_iterating = True
         self.ix = self.start
         return self
 
     def __len__(self) -> int:
+        """
+        :return: int, total number of items that this BasicRange iterates over
+        """
         return len(self.to_iter)
 
     def __next__(self) -> Typ.I:
+        """
+        :raises StopIteration: If there are no more items to iterate over.
+        :return: Any, the next element of the Sequence being iterated over.
+        """
         if self.is_iterating:
             next_value = self[self.ix]  # self.__getitem__(self.ix)
             self.is_iterating = self.has_next()
@@ -60,6 +70,9 @@ class BasicRange(Iterable):
             raise StopIteration()
 
     def has_next(self) -> bool:
+        """
+        :return: bool, True if any items remain to iterate over; else False
+        """
         return self.not_yet_at(self.ix, self.end)
 
 
@@ -107,13 +120,15 @@ def spliterate(parts: Iterable[str], ready_if:
                pop_ix: int = -1, join_on: str = " ") -> tuple[str, Any]:
     """ _summary_
 
-    :param parts: Iterable[str], _description_
+    :param parts: Iterable[str] to iteratively modify, check, and recombine
     :param is_not_ready: Callable[[str], bool], function that returns False \
         if join_on.join(parts) is ready to return and True if it still \
         needs to be modified further; defaults to Whittler.is_none
-    :param pop_ix: int,_description_, defaults to -1
-    :param join_on: str,_description_, defaults to " "
-    :return: _type_, _description_
+    :param pop_ix: int, index of item to remove from parts once per \
+        iteration; defaults to -1 (the last item)
+    :param join_on: str, delimiter to insert between parts; defaults to " "
+    :return: tuple[str, Any], the modified and recombined string built from \
+        parts and then something retrieved from within parts
     """
     gotten = get_target(parts[pop_ix])
     rejoined = join_on.join(parts)
@@ -125,32 +140,60 @@ def spliterate(parts: Iterable[str], ready_if:
     return rejoined, gotten
 
 
-class WhittleUntil(BasicRange):
+class ReadyChecker(BasicRange):
+    """ Context manager class to conveniently check once per iteration \
+        whether an item being modified is ready to return. """
 
-    def __init__(self, to_whittle: Typ.M, ready_if: Typ.Ready,
-                 find_in: Iterable[Typ.I], *ready_args: Typ.R,
-                 **ready_kwargs: Any) -> None:
-        super().__init__(list(find_in))
+    def __init__(self, to_check: Typ.M, iter_over: Iterable[Typ.I],
+                 ready_if: Typ.Ready, *ready_args: Typ.R, **ready_kwargs: Any
+                 ) -> None:
+        """
+        :param to_check: Any, the item to iteratively check the readiness of.
+        :param iter_over: Iterable[Any] to iterate over.
+        :param ready_if: Callable[[Any, *ready_args, **ready_kwargs], bool], \
+            function that returns either True if to_check is ready to return \
+            or False if it needs further modification.
+        :param ready_args: Iterable[_R] of positional arguments to pass \
+            into the ready_if function
+        :param ready_args: Mapping[str, Any] of keyword arguments to pass \
+            into the ready_if function
+        """
+        super().__init__(iter_over=list(iter_over))
         self.args = ready_args
         self.item_is_ready = ready_if
         self.kwargs = ready_kwargs
-        self.to_whittle = to_whittle
+        self.to_check = to_check
 
     def __enter__(self):
-        """ Must be explicitly defined here (not only in a superclass) for \
-            VSCode to realize that WhittleUntil(...) returns an instance of \
-            the WhittleUntil class.
+        """ Method called when entering the active block of a context manager.
 
-        :return: WhittleUntil, self.
+        Must be explicitly defined here (not only in a superclass) for \
+        VSCode to realize that ReadyChecker(...) returns an instance of \
+        the ReadyChecker class after importing gconanpy via poetry.
+
+        :return: ReadyChecker, self.
         """
         return self
 
     def __exit__(self, exc_type: type[BaseException] | None = None,
                  *_: Any) -> bool:
+        """ Method called when exiting the active block of a context manager.
+
+        :param exc_type: type[BaseException] called from within the block, \
+            or None if the block executed without raising any errs/exceptions
+        :return: bool, True if the block executed without error; else False
+        """
         return exc_type is None
 
     def __call__(self, item: Any) -> None:  # next_try
-        self.to_whittle = item
+        """ Save a thing to iteratively modify and check the readiness of.
 
-    def is_still_whittling(self) -> bool:  # is_not_ready
-        return self.is_iterating and not self.item_is_ready(self.to_whittle)
+        :param item: Any, object to check the readiness of.
+        """
+        self.to_check = item
+
+    def is_not_ready(self) -> bool:
+        """ 
+        :return: bool, False if there is no more need to iterate; else False.
+        """
+        return self.is_iterating and not self.item_is_ready(self.to_check)
