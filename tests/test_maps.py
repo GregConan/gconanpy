08814c02cc9d1ef6300f29f9a9fbb7542609e2a3
Greg Conan <gregmconan@gmail.com>
2025-04-22 20:58:48 -0700

    Updated test_maps.py
    - Updated `tests/test_maps.py` to modularize it and to validate prior commit's changes to `gconanpy/maps.py`
      - Added `MapTester` base class to abstract/modularize the functionality used in multiple of the specific `maps.py` class testers:
        - Changed `TestDotDicts.get_dot_dicts` to `MapTester.get_custom_dicts`
        - Moved `map_test` and `get_1s_dict` methods from `TestInvertionary` to `MapTester`
      - Added `TestDefaultionary` with methods to test the changes to `Defaultionary`'s `update` and `setdefaults` methods
      - Removed `TestDotDict.test_7` which tested `DotDict`'s compatibility with `dataclasses.dataclass` (no longer being pursued)
      - Changed `TestInvertionary` methods to test `Invertionary` methods on all descendants of `Invertionary`

--- tests/test_maps.py
+++ tests/test_maps.py
@@ -3,33 +3,112 @@
 """
 Greg Conan: gregmconan@gmail.com
 Created: 2025-04-07
-Updated: 2025-04-16
+Updated: 2025-04-22
 """
 # Import standard libraries
-from collections.abc import Generator
-from dataclasses import dataclass
+from collections.abc import Callable, Generator, Mapping
+import dataclasses
+from typing import Any
 
 # Import local custom libraries
-from gconanpy.maps import DotDict, Invertionary, LazyDotDict
+from gconanpy.maps import Defaultionary, DotDict, Invertionary, LazyDotDict
+from gconanpy.metafunc import AttributesOf
 from tests.testers import Tester
 
 
-class TestDotDicts(Tester):
-    TEST_CLASSES = (DotDict, LazyDotDict)
+class MapTester(Tester):
+    TEST_CLASSES: tuple[type[Mapping], ...]
 
-    def get_dot_dicts(self) -> Generator[type, None, None]:
+    def get_1s_dict(self) -> dict[str, int]:
+        return dict(a=1, b=1, c=1, d=1)
+
+    def get_custom_dicts(self) -> Generator[type, None, None]:
         self.add_basics()
-        for ddclass in self.TEST_CLASSES:
-            yield ddclass(self.adict)
+        for dict_class in self.TEST_CLASSES:
+            yield dict_class(self.adict)
+
+    def map_test(self, map_type: type, in_dict: dict, out_dict: dict,
+                 method_to_test: str, *method_args, **method_kwargs) -> None:
+        a_map = map_type(in_dict)
+        getattr(a_map, method_to_test)(*method_args, **method_kwargs)
+        self.check_result(a_map, map_type(out_dict))
+
+
+class TestInvertionary(MapTester):
+    TEST_CLASSES = (Invertionary, Defaultionary, DotDict, LazyDotDict)
 
     def test_1(self):
-        for dd in self.get_dot_dicts():
+        self.add_basics()
+        for dict_class in self.TEST_CLASSES:
+            self.map_test(dict_class, self.adict, {1: "a", 2: "b", 3: "c"},
+                          "invert")
+
+    def test_2(self):
+        for dict_class in self.TEST_CLASSES:
+            self.map_test(dict_class, self.get_1s_dict(), {1: "d"}, "invert")
+
+    def test_3(self):
+        for dict_class in self.TEST_CLASSES:
+            for holder_type in (list, set, tuple):
+                self.map_test(dict_class, self.get_1s_dict(),
+                              {1: holder_type(["a", "b", "c", "d"])},
+                              "invert", keep_collisions_in=holder_type)
+
+    def test_4(self):
+        for dict_class in self.TEST_CLASSES:
+            actual_err = None
+            try:
+                invertable = dict_class(a=dict(b=2))
+                invertable.invert(keep_collisions_in=list)
+            except TypeError as expected_err:
+                actual_err = expected_err
+            assert isinstance(actual_err, TypeError)
+
+
+class TestDefaultionary(MapTester):
+    TEST_CLASSES = (Defaultionary, DotDict, LazyDotDict)
+
+    def one_update_test(self, dfty: Defaultionary, expected_len: int,
+                        a_map: Mapping | None = None,
+                        **expected: Any) -> Defaultionary:
+        for copy in (False, True):
+            if a_map is None:
+                newd = dfty.update(**expected, copy=copy)
+            else:
+                newd = dfty.update(a_map, **expected, copy=copy)
+                expected.update(a_map)
+            if copy:
+                dfty = newd
+            self.check_result(len(dfty), expected_len)
+            for k, v in expected.items():
+                self.check_result(dfty[k], v)
+        return dfty
+
+    def test_update_1(self) -> None:
+        for dfty in self.get_custom_dicts():
+            self.one_update_test(dfty, 4, d=4)
+
+    def test_update_2(self) -> None:
+        for dfty in self.get_custom_dicts():
+            dfty = self.one_update_test(dfty, 4, dict(d=4))
+            self.one_update_test(dfty, 4, dict(a=3), c=1)
+
+    def test_setdefaults_1(self) -> None:
+        for dfty in self.get_custom_dicts():
+            dfty.setdefaults()
+
+
+class TestDotDicts(MapTester):
+    TEST_CLASSES = (DotDict, LazyDotDict)
+
+    def test_1(self):
+        for dd in self.get_custom_dicts():
             self.check_result(dd.a, 1)
             for k, v in self.adict.items():
                 self.check_result(getattr(dd, k), v)
 
     def test_2(self):
-        for dd in self.get_dot_dicts():
+        for dd in self.get_custom_dicts():
             self.check_result(len(dd), 3)
             del dd.b
             self.check_result([v for v in dd.values()], [1, 3])
@@ -37,7 +116,7 @@ class TestDotDicts(Tester):
             self.check_result(dd.PROTECTEDS in dd, False)
 
     def test_3(self):
-        for dd in self.get_dot_dicts():
+        for dd in self.get_custom_dicts():
             dd.five = 5
             self.check_result(dd["five"], 5)
             dd["six"] = 6
@@ -45,7 +124,7 @@ class TestDotDicts(Tester):
             assert self.cannot_alter(dd, "get")
 
     def test_4(self):
-        for dd in self.get_dot_dicts():
+        for dd in self.get_custom_dicts():
             dd.testdict = dict(a=dd)
             assert not isinstance(dd["testdict"], type(dd))
             dd.homogenize()
@@ -74,62 +153,5 @@ class TestDotDicts(Tester):
                     return f"sub{super().__getattr__(name)}"
 
             ddsc = DotDictSubClass(self.adict)
-            self.check_result({x: getattr(ddsc, x) for x in dir(ddsc)},
+            self.check_result({x: getattr(ddsc, x) for x in ddsc.keys()},
                               dict(a="sub1", b="sub2", c="sub3"))
-
-    def test_7(tester):
-        tester.add_basics()
-        for ddclass in tester.TEST_CLASSES:
-            @dataclass
-            class DDSubClass(ddclass):
-                # Input parameters without default values
-                name: str
-
-                # Attributes and input parameters with default values
-                description: str = "Description"
-                amount: int = 12
-                age: int = 21
-
-            class DDSubSubClass(DDSubClass):
-                def __init__(self, **kwargs):
-                    LazyDotDict.__init__(self)
-                    self.update(kwargs)
-                    self.setdefault("amount", 5)
-                    self.description = "something else"
-
-            ddssc = DDSubSubClass(name="something")
-            print(ddssc["age"])
-
-
-class TestInvertionary(Tester):
-    def get_1s_dict(self) -> dict[str, int]:
-        return dict(a=1, b=1, c=1, d=1)
-
-    def map_test(self, map_type: type, in_dict: dict, out_dict: dict,
-                 method_to_test: str, *method_args, **method_kwargs) -> None:
-        a_map = map_type(in_dict)
-        getattr(a_map, method_to_test)(*method_args, **method_kwargs)
-        self.check_result(a_map, map_type(out_dict))
-
-    def test_1(self):
-        self.add_basics()
-        self.map_test(Invertionary, self.adict, {1: "a", 2: "b", 3: "c"},
-                      "invert")
-
-    def test_2(self):
-        self.map_test(Invertionary, self.get_1s_dict(), {1: "d"}, "invert")
-
-    def test_3(self):
-        for holder_type in (list, set, tuple):
-            self.map_test(Invertionary, self.get_1s_dict(),
-                          {1: holder_type(["a", "b", "c", "d"])}, "invert",
-                          keep_collisions_in=holder_type)
-
-    def test_4(self):
-        actual_err = None
-        try:
-            invertable = Invertionary(a=dict(b=2))
-            invertable.invert(keep_collisions_in=list)
-        except TypeError as expected_err:
-            actual_err = expected_err
-        assert isinstance(actual_err, TypeError)
